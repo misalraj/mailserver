fec00caf09f4d7080e581ab242fd14f02ce33ef7
misal <misal@desktop.example.com>
2017-12-23 15:48:27 +0530

    setup file initial commit

--- server_setup.sh
+++ server_setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+mail_user=server_user
+mail_pwd=server_pwd
+
+sudo usermod -aG docker $USER
 sudo apt-get install -y docker
 sudo docker pull catatnight/postfix
+sudo docker run -p 25:25 -e maildomain=mail.example.com -e smtp_user=$mail_user:$mail_pwd -d catatnight/postfix
+sudo docker run -p 587:587 -e maildomain=mail.example.com -e smtp_user=$mail_user:$mail_pwd -v /path/to/certs:/etc/postfix/certs -d catatnight/postfix
