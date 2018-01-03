abcc5cbe41f6d3dd69bd8d0847382ded83954fec
misalraj <34789277+misalraj@users.noreply.github.com>
2018-01-03 22:56:32 +0530

    Update server_setup.sh

--- server_setup.sh
+++ server_setup.sh
@@ -7,3 +7,4 @@ sudo apt-get install -y docker
 sudo docker pull catatnight/postfix
 sudo docker run -p 25:25 -e maildomain=mail.example.com -e smtp_user=$mail_user:$mail_pwd -d catatnight/postfix
 sudo docker run -p 587:587 -e maildomain=mail.example.com -e smtp_user=$mail_user:$mail_pwd -v /path/to/certs:/etc/postfix/certs -d catatnight/postfix
+#command
